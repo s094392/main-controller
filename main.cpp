b7bc67856c45a14bf98f8a7661f70bbcca47963c
Muller Lee <s094392@gmail.com>
2022-12-14 03:59:14 +0800

    model.hpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+#include "model.hpp"
+
 #include <hiredis/hiredis.h>
 #include <iostream>
 #include <string>
@@ -17,48 +19,6 @@ redisContext *redis_init(std::string redis) {
     }
   }
   return c;
-}
-
-class Model {
-public:
-  std::string name;
-  int model_id;
-  int layer;
-  Model(int model_id, std::string name, int layer)
-      : model_id(model_id), name(name), layer(layer) {}
-};
-
-class Task {
-public:
-  Model *model;
-  int layer_id;
-  Task(Model *model, int layer_id) : model(model), layer_id(layer_id) {}
-};
-
-class ModelTask {
-public:
-  Model *model;
-  std::vector<Task> tasks;
-  ModelTask(Model *model) : model(model) {
-    tasks.reserve(model->layer);
-    for (int i = 0; i < model->layer; i++) {
-      tasks.push_back(Task(model, i));
-    }
-  }
-};
-
-void send_model(Task &task, redisContext *c) {
-  redisReply *reply;
-  std::string cmd = std::to_string(task.model->model_id) + " " +
-                    std::to_string(task.layer_id);
-  std::cout << cmd << std::endl;
-  reply = (redisReply *)redisCommand(c, "RPUSH foo %s", cmd.c_str());
-}
-
-void send_model_task(ModelTask &model_task, redisContext *c) {
-  for (auto &task : model_task.tasks) {
-    send_model(task, c);
-  }
 }
 
 int main() {
